12e59ceefc3639fac3fb035903a39737045a8631
Ayush kumar <krayush294@gmail.com>
2025-05-10 17:23:58 +0530

    final commit

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -17,8 +17,8 @@ export default function HeroSection() {
     />
    </div>
 
-   <div className="container mx-auto px-4 relative z-10">
-    <div className="max-w-2xl">
+   <div className="container mx-auto px-4 relative z-10 ">
+    <div className="max-w-2xl max-sm:text-center">
      <motion.h1
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
